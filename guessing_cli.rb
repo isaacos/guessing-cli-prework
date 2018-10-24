b5cd8bc340ff1c4009aae58a3ba467c49d368f4b
Isaac Oseas <oseasisaac91@gmail.com>
2018-10-24 04:13:02 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -14,11 +14,11 @@ def run_guessing_game
       break
     elsif user_guess == rand_num
       puts "You guessed the correct number!"
-      puts "Guess a number between 1 and 6."
+     
       user_guess = gets.chomp.to_i
     else
       puts "The computer guessed #{rand_num}."
-      puts "Guess a number between 1 and 6."
+     
       user_guess = gets.chomp.to_i
     end
 
